b8a7e72c20b473ec464a6b17341d362a917340cc
beanboobi <ansen131@gmail.com>
2024-06-13 18:12:53 +0800

    Items

--- WebAppProject/WebAppProject/obj/Release/net7.0/WebAppProject.AssemblyInfo.cs
+++ WebAppProject/WebAppProject/obj/Release/net7.0/WebAppProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b327bb0144daa4adb8d920b47a842b7869a0a779")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppProject")]
