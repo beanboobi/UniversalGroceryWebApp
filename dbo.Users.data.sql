d89bf611b6cb092a946ea98605337a176a5de66d
EugeneNgew1 <156151691+EugeneNgew1@users.noreply.github.com>
2024-06-17 21:45:50 +0800

    Delete dbo.Users.data.sql

--- dbo.Users.data.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO Users (Username, Password, Name, Email)
-VALUES ('testuser1', 'password1', 'Test User 1', 'testuser1@example.com');
-
-INSERT INTO Users (Username, Password, Name, Email)
-VALUES ('testuser2', 'password2', 'Test User 2', 'testuser2@example.com');
